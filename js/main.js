faa80ca09b269fc0861b7ca89e64afe6a7920e82
Nicksonder Examar <nextgendev12@gmail.com>
2020-12-01 22:28:59 -0500

    I'm done with the Header, Jumbo, About and Trainers section.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!!!');
+// Video
+
+var playBtn = document.querySelector(".play-btn");
+var videoBox = document.querySelector(".video-box");
+var exitBox = document.querySelector(".exit-box");
+
+playBtn.onclick = function () {
+	videoBox.classList.add("active");
+}
+
+exitBox.onclick = function () {
+	videoBox.classList.remove("active");
+}
